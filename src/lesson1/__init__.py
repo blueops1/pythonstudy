84c4179046c3a403b16e623cfaf93c3e1d57b681
blueops1 <hyzfgjj@163.com>
2018-07-02 23:17:43 +0800

    dict study

--- src/lesson1/__init__.py
+++ src/lesson1/__init__.py
@@ -2,3 +2,7 @@ print('This is lesson 1')
 print('egit is successful!')
 print('retry the key')
 print('this line is type at home_pc!')
+dictofpeople = [['necho','12345678910'],['alise','98765432101']]
+for people in dictofpeople:
+    for name in people:
+        print(name)
