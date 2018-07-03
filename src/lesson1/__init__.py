dbb9b9ba7217f54faf8dcdc6d549cb898344386c
blueops1 <hyzfgjj@163.com>
2018-07-03 23:19:47 +0800

    网络爬虫2

--- src/lesson1/__init__.py
+++ src/lesson1/__init__.py
@@ -12,4 +12,16 @@ x = len(data)
 import re
 pat = "<title>(.*?)</title>"
 rst = re.findall(pat,data,re.S)
-print(rst)       
+print(rst)      
+urllib.request.urlretrieve("https://www.jd.com/", filename="d:/test.html") 
+
+url = "https://www.qiushibaike.com"
+
+opener = urllib.request.build_opener()
+
+UA = ("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
+#中国
+opener.addheaders=[UA]
+urllib.request.install_opener(opener)
+data = urllib.request.urlopen(url).read().decode("utf-8","ignore")
+print(data)
