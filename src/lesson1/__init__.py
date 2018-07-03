30084332fa355b8f3282a8773d47d8577727328b
blueops1 <hyzfgjj@163.com>
2018-07-03 13:37:42 +0800

    网络爬虫初体验

--- src/lesson1/__init__.py
+++ src/lesson1/__init__.py
@@ -6,3 +6,10 @@ dictofpeople = [['necho','12345678910'],['alise','98765432101']]
 for people in dictofpeople:
     for name in people:
         print(name)
+import urllib.request
+data = urllib.request.urlopen("http://www.baidu.com").read().decode("utf-8","ignore")
+x = len(data)
+import re
+pat = "<title>(.*?)</title>"
+rst = re.findall(pat,data,re.S)
+print(rst)       
